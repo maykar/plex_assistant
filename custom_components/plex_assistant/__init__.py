c676ffbfd6f4597cdaa1ba7833ec89ea4268435f
maykar <25127328+maykar@users.noreply.github.com>
2020-09-21 17:16:21 -0400

    docstrings and comments

--- custom_components/plex_assistant/__init__.py
+++ custom_components/plex_assistant/__init__.py
@@ -72,22 +72,23 @@ async def async_setup(hass, config):
     zconf = await async_get_instance(hass)
     localize = LOCALIZE[lang] if lang in LOCALIZE.keys() else LOCALIZE["en"]
 
+    # Find or create the directory to hold TTS error MP3s.
     dir = hass.config.path() + "/www/plex_assist_tts/"
     if tts_errors and not os.path.exists(dir):
         os.makedirs(dir, mode=0o777)
 
     def pa_executor(url, token, aliases):
-        PA = PlexAssistant(url, token, aliases)
-        PA.plex_client_update()
-        return PA
+        return PlexAssistant(url, token, aliases)
 
     PA = await hass.async_add_executor_job(pa_executor, url, token, aliases)
 
     def cc_callback(chromecast):
+        """get_chromecasts() callback function."""
         PA.chromecasts[chromecast.device.friendly_name] = chromecast
 
     get_chromecasts(blocking=False, callback=cc_callback, zeroconf_instance=zconf)
 
+    # First update of sensor.
     def sensor_executor():
         time.sleep(5)
         update_sensor(hass, PA, sensor)
@@ -211,48 +212,70 @@ async def async_setup(hass, config):
 
 
 class PlexAssistant:
-    """Hold our libraries, devices, etc."""
+    """Main class for interacting with the Plex server.
+
+    Args:
+        url (str): URL to connect to server.
+        token (str): X-Plex-Token used for authenication.
+        aliases (dict): Alternate names assigned to devices.
+
+    Attributes:
+        server (:class:`~plexapi.server.PlexServer`): The Plex server.
+        plex: The main library for all media, recentlyAdded, onDeck, etc.
+        lib (dict): All video (seperated into sections), media titles, & time last updated.
+        aliases (dict): Alternate names assigned to devices.
+        alias_names (list): List of alias names.
+        chromecasts (dict): All connected Google cast devices.
+        chromecast_names (list): List of Google cast device names.
+        plex_clients (list): List of connected Plex client objects.
+        plex_client_names (list): List of Plex client titles.
+        plex_client_ids (list): List of Plex client machine IDs.
+        device_names (list): Combined list of alias, chromecast, and plex client names.
+    """
 
     from plexapi.server import PlexServer
     from datetime import datetime
 
-    chromecasts = {}
-    plex_clients = {}
+    chromecasts = {}  # Updated on load and on call with get_chromecasts().
 
     def __init__(self, url, token, aliases):
         self.server = PlexServer(url, token)
         self.plex = self.server.library
-        self.get_libraries()
+        self.lib = self.get_libraries()
         self.aliases = aliases
         self.alias_names = list(aliases.keys()) if aliases else []
+        self.plex_clients = self.server.clients()
 
     @property
     def chromecast_names(self):
+        """Returns list of Chromcast names"""
         return list(self.chromecasts.keys())
 
     @property
     def plex_client_names(self):
+        """Returns list of Plex client names"""
         return [client.title for client in self.plex_clients]
 
     @property
-    def device_names(self):
-        return self.chromecast_names + self.plex_client_names + self.alias_names
-
-    @property
     def plex_client_ids(self):
+        """Return a list of current Plex client's machine IDs."""
         return [client.machineIdentifier for client in self.plex_clients]
 
+    @property
+    def device_names(self):
+        """Return list of devices and aliases names"""
+        return self.chromecast_names + self.plex_client_names + self.alias_names
+
     def plex_client_update(self):
-        self.plex_clients = self.server.clients() if self.server else []
+        """Get currently connected Plex clients."""
+        self.plex_clients = self.server.clients()
 
     def get_libraries(self):
-        """Return Plex libraries, lib contents, media titles, & time updated."""
+        """Update library contents, media titles, & set time updated."""
 
         self.plex.reload()
-        movies = self.plex.search(libtype="movie")
-        movies.sort(key=lambda x: x.addedAt or x.updatedAt)
-        shows = self.plex.search(libtype="show")
-        shows.sort(key=lambda x: x.addedAt or x.updatedAt)
+        movies = self.plex.search(libtype="movie", sort="addedAt:desc")
+        shows = self.plex.search(libtype="show", sort="addedAt:desc")
 
         self.lib = {
             "movies": movies,

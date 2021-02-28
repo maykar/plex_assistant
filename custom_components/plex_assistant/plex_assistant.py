a7b87edaad3c17182934b61df102f0b1c5533ec0
Ryan Meek <25127328+maykar@users.noreply.github.com>
2021-02-28 10:03:28 -0500

    only use titles, add section ids

--- custom_components/plex_assistant/plex_assistant.py
+++ custom_components/plex_assistant/plex_assistant.py
@@ -9,22 +9,35 @@ class PlexAssistant:
         self.media = {}
         self.start_script_keys = start_script_keys
         self.update_libraries()
+        self.tv_id = self.get_section_id("show")
+        self.movie_id = self.get_section_id("movie")
+        self.music_id = self.get_section_id("artist")
 
     @property
     def device_names(self):
         names = list(self.devices.keys()) + self.start_script_keys
         return list(dict.fromkeys(names))
 
+    @property
+    def section_id(self):
+        return {
+            "movie": self.movie_id,
+            "show": self.tv_id,
+            "season": self.tv_id,
+            "episode": self.tv_id,
+            "artist": self.music_id,
+            "album": self.music_id,
+            "track": self.music_id,
+        }
+
     def update_libraries(self):
         self.library.reload()
         self.media["all_titles"] = []
-
         for item in ["show", "movie", "artist", "album", "track"]:
-            self.media[f"{item}s"] = self.library.search(libtype=item, sort="addedAt:desc")
-            self.media[f"{item}_titles"] = [x.title for x in self.media[f"{item}s"]]
+            self.media[f"{item}_titles"] = [x.title for x in self.library.search(libtype=item, sort="addedAt:desc")]
             self.media["all_titles"] += self.media[f"{item}_titles"]
-
-        self.media["playlists"] = self.server.playlists()
-        self.media["playlist_titles"] = [playlist.title for playlist in self.media["playlists"]]
-
+        self.media["playlist_titles"] = [x.title for x in self.server.playlists()]
         self.media["updated"] = datetime.now()
+
+    def get_section_id(self, section):
+        return self.library.search(libtype=section, limit=1)[0].librarySectionID
